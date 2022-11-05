3afffbf0e2e61edf84749c7381c1253cad2d18e1
abivarma.kandiah <abivarman.kandiah@etu.hesge.ch>
2022-11-05 22:12:02 +0100

    Finished basic buffer to png

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod render;
 
-
-fn main() {
-    println!("Hello, world!");
+fn main() 
+{
+    let mut r = render::Render::new("render/test.png".to_string(), (1024, 1024));
+    r.export_png();
 }
--- src/render.rs
+++ src/render.rs
@@ -1,6 +1,62 @@
 use png;
 
+// For reading and opening files
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+
+
 pub struct Render
 {
-	pub buffer	:	[[(u8,u8,u8,u8);1024];1024]	//RGBA
+	pub path	:	String,
+	pub r_size	:	(usize, usize),
+	pub buffer	:	Vec<Vec<(u8, u8, u8, u8)>>,	//RGBA
+}
+
+impl Render
+{
+	pub fn new(path: String, img_size: (usize, usize)) -> Self
+	{
+		Self {path: path, r_size: img_size, buffer: vec![vec![(0, 0, 0, 255); img_size.0]; img_size.1]}
+	}
+
+	pub fn buffer_to_1d(&mut self) -> Vec<u8>	
+	{
+		let mut d_array: Vec<u8> = vec![];
+		for i in 0..1024
+		{
+			for j in 0..1024
+			{
+				d_array.push(self.buffer[i][j].0);
+				d_array.push(self.buffer[i][j].1);
+				d_array.push(self.buffer[i][j].2);
+				d_array.push(self.buffer[i][j].3);
+			}
+		}
+		return d_array;
+	}
+
+	pub fn export_png(&mut self)
+	{
+		let path = Path::new(&self.path);
+		let file = File::create(path).unwrap();
+		let ref mut w = BufWriter::new(file);
+
+		let mut encoder = png::Encoder::new(w, self.r_size.0 as u32, self.r_size.1 as u32); // Width and height
+		encoder.set_color(png::ColorType::Rgba);
+		encoder.set_depth(png::BitDepth::Eight);
+		encoder.set_source_gamma(png::ScaledFloat::from_scaled(45455)); // 1.0 / 2.2, scaled by 100000
+		encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));     // 1.0 / 2.2, unscaled, but rounded
+		let source_chromaticities = png::SourceChromaticities::new(     // Using unscaled instantiation here
+			(0.31270, 0.32900),
+			(0.64000, 0.33000),
+			(0.30000, 0.60000),
+			(0.15000, 0.06000)
+		);
+		encoder.set_source_chromaticities(source_chromaticities);
+		let mut writer = encoder.write_header().unwrap();
+		
+		let data = self.buffer_to_1d();
+		writer.write_image_data(&data).unwrap(); // Save
+	}
 }
